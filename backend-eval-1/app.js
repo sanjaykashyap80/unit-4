6fc1b828b312dda9e9063da914839e07cf0a815b
sanjaykashyap80 <91534908+sanjaykashyap80@users.noreply.github.com>
2021-11-29 16:55:44 +0530

    eval

---
+++ backend-eval-1/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const app = express();
+const jobs = require("./jobs.json");
+const wfmjobs = require("./wfmjobs.json");
+const noticePeriod2Month = require("./noticeP2Mjobs.json");
+const company_details = require('./company.json')
+
+
+app.use(express.json());
+
+app.get("/jobs/:city",(req,res)=>{
+    const cityjobs = jobs.filter((job)=> job.city === req.params.city);
+    res.send(cityjobs);
+
+})
+
+app.get("/:company_name", (req,res)=>{
+    const detail = company_details.filter((det)=> det.company_name === req.params.company_name);
+    res.send(detail);
+})
+
+app.get("/noticeP2M",(req,res)=>{
+    res.send({noticePeriod2Month});
+})
+
+
+app.get("/wfmjobs",(req,res)=>{
+    res.send({wfmjobs});
+})
+
+app.listen(2222, ()=>{
+    console.log("Listening on 22222");
+})
